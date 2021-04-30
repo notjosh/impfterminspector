a8aa7b6bb77f5af6a2db034138a2325583256ce5
Joshua May <notjosh@gmail.com>
2021-04-30 15:13:59 +0200

    Added: Tempelhof: Moderna

--- src/doctolib/booking-sources.ts
+++ src/doctolib/booking-sources.ts
@@ -1,4 +1,4 @@
-import { InsuranceType } from './insurance-types';
+import { InsuranceType, insuranceTypes } from './insurance-types';
 import { BookingSource } from './types';
 import * as VaccinationSite from './vaccination-sites';
 import { VaccinationType } from './vaccination-types';
@@ -64,6 +64,40 @@ export const bookingSources: BookingSource[] = [
     site: VaccinationSite.tempelhof,
     insurance: InsuranceType.PRIVATE,
     vaccination: VaccinationType.ASTRAZENECA,
+  },
+  {
+    site: VaccinationSite.tempelhof,
+    insurance: InsuranceType.PUBLIC,
+    vaccination: VaccinationType.MODERNA,
+    override: {
+      agendaIds: [
+        '467901',
+        '467933',
+        '467893',
+        '467894',
+        '467897',
+        '467898',
+        '467899',
+        '467895',
+        '467896',
+        '467900',
+        '467908',
+        '467912',
+        '467903',
+        '467905',
+        '467906',
+        '467907',
+        '467910',
+        '467911',
+        '467934',
+        '467935',
+        '467936',
+        '467937',
+        '467938',
+        '467939',
+        '467940',
+      ],
+    },
   },
 
   {
